d3ac786ca99ba18c986aed468e4add930c87990c
GabrielSMarques <gabriel1997.marques97@gmail.com>
2018-10-03 18:08:15 -0300

    Compila aí mirandão

--- unidadeProcessamento.sv
+++ unidadeProcessamento.sv
@@ -1,8 +1,22 @@
-module unidadeProcessamento(input clk, PCSrc, ALUFunct, ALUSrcB, ALUSrcA, LoadRegA, LoadRegB, LoadALUOut, PCWrite, LoadIR, IMemLoad, DMemLoad, LoadMDR, inst);
-	logic clk, PCSrc, ALUSrcA, LoadRegA, LoadRegB, LoadALUOut, PCWrite, LoadIR, IMemLoad, DMemLoad, LoadMDR;
-	logic [2:0] ALUFunct, ALUSrcB;
-	logic [31:0] inst;
+module unidadeProcessamento(
+	input logic clk, 
+	input logic PCSrc, 
+	input logic [2:0] ALUFunct, 
+	input logic [2:0] ALUSrcB, 
+	input logic ALUSrcA, 
+	input logic LoadRegA, 
+	input logic LoadRegB, 
+	input logic LoadALUOut, 
+	input logic PCWrite, 
+	input logic LoadIR, 
+	input logic IMemLoad, 
+	input logic DMemLoad, 
+	input logic LoadMDR, 
+	input logic Reset,
+	output logic [31:0] inst);
 
-	reg [63:0] PC;
-		
+	wire [63:0] PCIn, PCOut;
+
+	Registrador64 pc(clk, Reset, PCSrc, PCIn, PCOut);
+
 endmodule 
