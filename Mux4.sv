fd1e54bd25138a938393c51a1b417561ce8e36ac
Rafael Carneiro Reis de Souza <rcrs4@cin.ufpe.br>
2018-10-04 19:42:14 -0300

    Unidade de controle ok, sub testado.

--- Mux4.sv
+++ Mux4.sv
@@ -1,15 +1,19 @@
-module Mux4(input logic [3:0]Control,input logic [63:0]In1,input logic [63:0] In2,input logic [63:0] In3,input logic [63:0] In4,output logic [63:0] Out);
-	//input logic [3:0]Control, [63:0]In1, [63:0]In2, [63:0]In3, [63:0]In4;
-	//output logic [63:0]Out;
+module Mux4(input logic [1:0]Control,input logic [63:0]In1, input logic [63:0] In2, input logic [63:0]In3, In4,output logic [63:0] Out);
 	always_comb begin
-		if(Control == 3'b00) 
-			Out = In1;
-		else if(Control == 3'b01)
-			Out = In2;
-		else if(Control == 3'b10)
-			Out = In3;
-		else if(Control == 3'b11)
-			Out = In4;
+		case(Control)
+			2'b00:		begin
+			Out <= In1;
+			end
+			2'b01:		begin
+			Out <= In2;
+			end
+			2'b10:		begin
+			Out <= In3;
+			end
+			2'b11:		begin
+			Out <= In4;
+			end
+	endcase
 	end
 
 endmodule // mux
