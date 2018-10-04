91ea6e5d1ed0356a5acd2071f7af4c4e4465130b
Luis Felipe <luisfelip80@gmail.com>
2018-10-03 21:24:17 -0300

    Compila, funciona? Não sei.

--- processor.sv
+++ processor.sv
@@ -1,47 +1,66 @@
-module uc (input logic clock, input logic[31:0] instruction, PCWrite, PCWriteCond, BranchOp, PCSrc, ALUFunct, ALUSrcB, ALUSrcA, LoadRegA, LoadRegB, LoadALUOut, LoadMDR, DMemWrite, IMemWrite, LoadIR, MemToReg, WriteReg)
-	output logic PCWrite, PCWriteCond, BranchOp, PCSrc, ALUSrcA, LoadRegA, LoadRegB, LoadALUOut, LoadMDR, DMemWrite, IMemWrite, LoadIR, WriteReg;	
-	output logic[1:0] ALUSrcB, MemToReg;
-	output logic[2:0] ALUFunct;
+module uc (input logic clk, input logic[31:0] instruction,output logic PCWrite,
+	output logic PCWriteCond,output logic BranchOp,output logic PCSrc,output logic [2:0] ALUFunct,
+	output logic [1:0] ALUSrcB,output logic ALUSrcA,output logic LoadRegA,output logic LoadRegB,
+	output logic LoadALUOut,output logic LoadMDR,output logic DMemWrite,output logic IMemWrite,output logic LoadIR,
+	output logic [1:0] MemToReg,output logic WriteReg);
 
 	reg [4:0]state;
-	parameter init_state = 0;
-	parameter decod = 1;
-	parameter cal_offset = 2;
-	parameter sum_reg = 3;
-	parameter sub_reg = 4;
-	parameter read_mem = 5;
-	parameter write_mem = 6;
-	parameter lui = 7;
-	parameter beq_wpc = 8;
-	parameter bne_wpc = 9;
-	parameter ld_wreg = 10;
-	parameter add_wreg = 11;
+	parameter init_state = 0; //estado 0
+	parameter decod = 1;  // estado 1
+	parameter cal_offset = 2; //
+	parameter sum_reg = 3; //
+	parameter sub_reg = 4; //
+	parameter read_mem = 5;// 
+	parameter write_mem = 6;//
+	parameter lui = 7; //
+	parameter beq_wpc = 8;//
+	parameter bne_wpc = 9;//
+	parameter ld_wreg = 10;//
+	parameter add_wreg = 11;//
 
-	always_ff@(posedge clk) begin
+	always @(posedge clk) begin
 		case(state)
 			init_state: begin
-				IMemWrite <= 1;
-				ALUSrcA <= 1;
-				DMemWrite <= 0;
-				ALUSrcB <= 2'b01;
-				ALUFunct <= 3'b001
-				PCWrite <= 1;
-				PCSrc <= 0;
-				state <= decod;
-			end
-			decod: begin
-				LoadRegA <= 1;
-				LoadRegB <= 1;
-				LoadIR <= 1;
 
+				PCWrite <= 1;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b001;
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b01;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				state <= decod;
+				end
+			decod: begin
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b001;
 				ALUSrcA <= 0;
 				ALUSrcB <= 2'b11;
-				ALUFunct <= 3'b001;
-				LoadALUOut <= 1;
-				case([6:0]instruction)
+				LoadRegA <= 1;
+				LoadRegB <= 1;
+				LoadALUOut <=1;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 1;
+
+
+				case(instruction[6:0])
 					7'b0110011: //type r
 					begin
-						case([31:24]instruction)
+						case(instruction[31:24])
 							7'b0000000: // add
 							begin
 								ALUSrcA <= 2'b01;
@@ -92,13 +111,13 @@ module uc (input logic clock, input logic[31:0] instruction, PCWrite, PCWriteCon
 					end
 					7'b1100111: //type sb
 					begin
-						case([10:7]instruction)
+						case(instruction[10:7])
 							3'b000: // BEQ
 							begin 
 								ALUSrcA <= 2'b01;
 								ALUSrcB <= 0;
 								ALUFunct <= 3'b010;
-								PCWriteCond = 1
+								PCWriteCond = 1;
 								PCSrc = 2'b01;
 								BranchOp = 0;
 								state <= init_state; //volta pro começo 	
@@ -116,40 +135,203 @@ module uc (input logic clock, input logic[31:0] instruction, PCWrite, PCWriteCon
 						endcase
 					end		
 				endcase
-			end //end do decod
+				end
 			cal_offset: begin
-
-						end 
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b001;
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b10;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=1;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				case (instruction[6:0]) // sai de offset e vai para umas das funções
+					7'b0100011: //type s
+					begin
+						state <= write_mem; //calcula o OFFSET para o LOAD, STORE, E ADDI
+					end
+					7'b0010011: //type i (ADDI)
+					begin
+						state <= add_wreg; //calcula o OFFSET para o LOAD, STORE, E ADDI
+					end
+					7'b0000011: //type i (LD)
+					begin
+						state <= read_mem; //calcula o OFFSET para o LOAD, STORE, E ADDI
+					end
+					endcase
+				end 
 			sum_reg: begin
-
-					 end	
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b001;
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <= 1;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				state <= add_wreg;
+				end	
 			sub_reg: begin
-
-					 end	
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b010;
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=1;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				state <= add_wreg;
+				end	
 			read_mem: begin
-
-					  end
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b000;
+				ALUSrcA <= 0;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 1;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				state <= ld_wreg;
+				end
 			write_mem: begin
-
-					   end
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b000;
+				ALUSrcA <= 0;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 1;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				state<=init_state;
+				end
 			lui: begin
-
+				PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b000;
+				ALUSrcA <= 0;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 1;
+				MemToReg <= 2'b10;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				state <=init_state;
 				 end
-
 		 	beq_wpc: begin
-
-		 			 end
-		 			 
+		 		PCWrite <= 0;
+				PCWriteCond <= 1;
+				PCSrc <= 1;
+				ALUFunct <= 3'b010;
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				BranchOp <= 0;
+				state <= init_state;
+		 			 end	 
 		 	bne_wpc: begin
-		 			 
-		 			 end
-
+		 		PCWrite <= 0;
+				PCWriteCond <= 1;
+				PCSrc <= 1;
+				ALUFunct <= 3'b010;
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 0;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				BranchOp <= 1;
+				state <= init_state;
+		 			end
 		 	ld_wreg: begin
-		 			 
+		 		PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b000;
+				ALUSrcA <= 0;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 1;
+				MemToReg <= 1;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				BranchOp <= 0;
+				state<= init_state;
 		 			 end
-
 		 	add_wreg: begin
-
+		 		PCWrite <= 0;
+				PCWriteCond <= 0;
+				PCSrc <= 0;
+				ALUFunct <= 3'b000;
+				ALUSrcA <= 0;
+				ALUSrcB <= 2'b00;
+				LoadRegA <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				WriteReg <= 1;
+				MemToReg <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				BranchOp <= 0;
+				state<= init_state;
 		 			  end		 	
 		endcase 
 	end
