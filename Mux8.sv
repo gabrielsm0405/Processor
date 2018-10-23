c5a3a037e145c15667ac028928ab2956d3e14cf9
Rafael Carneiro Reis de Souza <rcrs4@cin.ufpe.br>
2018-10-22 22:29:37 -0300

    Mux de acordo com a CPU

--- Mux8.sv
+++ Mux8.sv
@@ -1,6 +1,6 @@
-module Mux8 (input logic [2:0]s, [63:0]In1, [63:0]In2,[63:0]In3,[63:0]In4,[63:0]In5, output logic [63:0] Out);
+module Mux8 (input logic [2:0]Control, logic [63:0]In1,logic [63:0]In2,logic [63:0]In3,logic [63:0]In4,logic [63:0]In5, output logic [63:0] Out);
 	always_comb begin
-		case(s)
+		case(Control)
 			3'b000: begin
 				Out <= In1;
 			end
