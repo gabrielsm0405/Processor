bdaa698e024368c010d163aec1287cccdd7fa87c
Pedro H Silva <33045260+phdosilva@users.noreply.github.com>
2018-10-25 15:02:53 -0300

    JALR funcionando!!!

--- unidadeControle.sv
+++ unidadeControle.sv
@@ -47,8 +47,10 @@ module unidadeControle (
 	parameter srai = 21;
 	parameter nothing = 22;
 	parameter excecao = 23;
-	parameter jal_register = 24;
-	parameter jal_offset = 25;
+	parameter jal_jalr_register = 24;
+	parameter jal_jalr_offset = 25;
+	parameter jalr_loadReg = 26;
+	parameter jalr_opImmReg = 27;
 
 	parameter Rtype = 7'b0110011;
 	parameter Stype = 7'b0100011;
@@ -203,6 +205,9 @@ module unidadeControle (
 					SBtype: //outros branchs - type sb
 					begin
 						case(instruction[14:12]) // funct3
+							3'b000: begin// JALR
+								state <= jal_jalr_register;
+							end
 							3'b001: // BNE
 							begin
 								state <= bne_wpc; //volta pro comeÃ§o 	
@@ -221,7 +226,7 @@ module unidadeControle (
 						endcase
 					end
 					Jal: begin
-						state <= jal_register;
+						state <= jal_jalr_register;
 					end	
 					default: begin
 						state <= excecao;
@@ -604,7 +609,7 @@ module unidadeControle (
 			nothing: begin
 				//Faz nada
 			end
-			jal_register: begin
+			jal_jalr_register: begin
 				MemToReg <= 3'b011;
 				WriteReg <= 1;
 
@@ -623,9 +628,16 @@ module unidadeControle (
 				LoadIR <= 0;
 				BranchOp <= 0;
 				
-				state <= jal_offset;		
+				case(instruction[6:0])
+					SBtype: begin
+						state <= jalr_loadReg;
+					end
+					default begin
+						state <=jal_jalr_offset;
+					end
+				endcase		
 			end
-			jal_offset: begin
+			jal_jalr_offset: begin
 				PCWrite <= 1;
 				PCSrc <= 1'b1;
 
@@ -645,6 +657,50 @@ module unidadeControle (
 				BranchOp <= 0;
 
 				state <= the_next_episode;
+			end
+			jalr_loadReg: begin
+				LoadRegA <= 1;
+
+
+				MemToReg <= 0;
+				WriteReg <= 0;
+				PCWriteCond <= 0;
+				ALUFunct <= 3'b000;
+				ALUSrcA <= 0;
+				ALUSrcB <= 2'b00;
+				PCWrite <= 0;
+				PCSrc <= 0;
+				LoadRegB <= 0;
+				LoadALUOut <=0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				BranchOp <= 0;
+
+				state <= jalr_opImmReg;
+			end
+			jalr_opImmReg: begin
+				ALUSrcA <= 1;
+				ALUSrcB <= 2'b11;
+				ALUFunct <= 3'b001;
+				LoadALUOut <= 1;
+				
+
+				LoadRegA <= 0;
+				MemToReg <= 0;
+				WriteReg <= 0;
+				PCWriteCond <= 0;
+				PCWrite <= 0;
+				PCSrc <= 0;
+				LoadRegB <= 0;
+				LoadMDR <= 0;
+				DMemWrite <= 0;
+				IMemWrite <= 0;
+				LoadIR <= 0;
+				BranchOp <= 0;
+
+				state <= jal_jalr_offset;
 			end
 			excecao: begin
 				
