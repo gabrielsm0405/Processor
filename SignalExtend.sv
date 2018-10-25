59c97f55d62b6d97812fdf004905122056e710f2
Pedro H Silva <33045260+phdosilva@users.noreply.github.com>
2018-10-24 21:32:43 -0300

    JAL-Alter. feitas no signalextend mas nada testado

--- SignalExtend.sv
+++ SignalExtend.sv
@@ -42,33 +42,69 @@ module SignalExtend(input logic [31:0] Inst, output logic [63:0] Out);
 					end
 				endcase
 			end
-			7'b1100111: begin
-				Out[10] <= Inst[7];
-				Out[3:0] <= Inst[11:8];
-				Out[9:4] <= Inst[30:25];
-				Out[11] <= Inst[31];
-				case(Inst[31])
-					1: begin
-						Out[63:12] <=  52'b1111111111111111111111111111111111111111111111111111;
+			7'b1100111: begin //tipo i e tipo sb
+				case(Inst[14:12])
+					3'b000: begin //jalr
+						Out[11:0] <= Inst[31:20];
+						case(Inst[31])
+							1: begin
+								Out[63:12] <=  52'b1111111111111111111111111111111111111111111111111111;
+							end
+							default:
+							begin
+								Out[63:12] <= 52'b0000000000000000000000000000000000000000000000000000;
+							end
+						endcase
+					end // 3'b000:
+					default: begin 
+						Out[11] <= Inst[7];
+						Out[4:1] <= Inst[11:8];
+						Out[10:5] <= Inst[30:25];
+						Out[12] <= Inst[31];
+						Out[0] <= 1'b0;
+						case(Inst[31])
+							1: begin
+								Out[63:13] <=  51'b1111111111111111111111111111111111111111111111111111;
+							end
+							default:
+							begin
+								Out[63:13] <= 51'b0000000000000000000000000000000000000000000000000000;
+							end
+						endcase
 					end
-					default:
-					begin
-						Out[63:12] <= 52'b0000000000000000000000000000000000000000000000000000;
-					end
-				endcase
+				endcase // Inst[14:12]
+					
 			end
 			7'b0110111: begin
 				Out[11:0] <= 12'b000000000000;
 				Out[31:12] <= Inst[31:12];
 				case(Inst[31]) 
 					1: begin
-						Out[63:32] <=  44'b11111111111111111111111111111111111111111111;
+						Out[63:32] <= 32'b11111111111111111111111111111111;
 					end
 					default:
 					begin
-						Out[63:32] <= 44'b00000000000000000000000000000000000000000000;
+						Out[63:32] <= 32'b00000000000000000000000000000000;
 					end
 				endcase
+			end
+			7'b1101111: begin //jal
+				case(Inst[31])
+					1: begin
+						Out[63:21] <= 43'b1111111111111111111111111111111111111111111;
+					end
+					default:
+					begin
+						Out[63:21] <= 43'b0000000000000000000000000000000000000000000;
+					end
+				endcase
+
+				Out[20] <= Inst[31];
+				Out[10:1] <= Inst[30:21];
+				Out[11] <= Inst[20];
+				Out[19:12] <= Inst[19:12];
+
+				Out[0] <=1'b0;
 			end
 		endcase
 		
